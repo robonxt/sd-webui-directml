0d807d33bd7c1df09edbea4ded3430998fa80025
robonxt <8854735+robonxt@users.noreply.github.com>
2023-06-29 12:20:56 -0700

    update webui-user

--- webui-user.bat
+++ webui-user.bat
@@ -5,6 +5,6 @@ set PYTHON=
 set GIT=
 set VENV_DIR=
 set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6,max_split_size_mb:128
-set COMMANDLINE_ARGS=--backend directml --disable-nan-check --no-download-sd-model --no-gradio-queue --enable-insecure-extension-access --medvram --always-batch-cond-uncond --no-half --precision full --upcast-sampling --use-cpu CLIP BLIP interrogate gfpgan bsrgan esrgan scunet codeformer --opt-split-attention --sub-quad-q-chunk-size 512 --sub-quad-kv-chunk-size 512 --sub-quad-chunk-threshold 80 --update-all-extensions --update-check --listen 
+set COMMANDLINE_ARGS=--backend directml --disable-nan-check --no-download-sd-model --gradio-queue --enable-insecure-extension-access --medvram --always-batch-cond-uncond --no-half --precision full --upcast-sampling --use-cpu CLIP BLIP interrogate gfpgan bsrgan esrgan scunet codeformer --opt-split-attention --sub-quad-q-chunk-size 512 --sub-quad-kv-chunk-size 512 --sub-quad-chunk-threshold 80 --update-all-extensions --update-check --listen --api --port 7860
 
 call webui.bat
