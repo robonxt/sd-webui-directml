c7e7b2aee90e4745280bfb277e0c1cec36164c70
robonxt <8854735+robonxt@users.noreply.github.com>
2023-07-07 14:22:02 -0700

    reverted previous change. (it breaks for me, generates only black images)
    Signed-off-by: robonxt <8854735+robonxt@users.noreply.github.com>

--- webui-user.bat
+++ webui-user.bat
@@ -5,6 +5,6 @@ set GIT=
 set VENV_DIR=
 set SAFETENSORS_FAST_GPU=1
 set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6,max_split_size_mb:128
-set COMMANDLINE_ARGS=--backend directml --disable-nan-check --skip-torch-cuda-test --no-download-sd-model --enable-insecure-extension-access --medvram --precision full --upcast-sampling --use-cpu CLIP BLIP interrogate gfpgan bsrgan esrgan scunet codeformer --sub-quad-q-chunk-size 512 --sub-quad-kv-chunk-size 512 --sub-quad-chunk-threshold 80 --update-all-extensions --skip-version-check --listen --port 7860
+set COMMANDLINE_ARGS=--backend directml --disable-nan-check --skip-torch-cuda-test --opt-sub-quad-attention --no-download-sd-model --enable-insecure-extension-access --medvram --precision full --upcast-sampling --use-cpu CLIP BLIP interrogate gfpgan bsrgan esrgan scunet codeformer --sub-quad-q-chunk-size 512 --sub-quad-kv-chunk-size 512 --sub-quad-chunk-threshold 80 --update-all-extensions --skip-version-check --listen --port 7860
 
 call webui.bat %*
