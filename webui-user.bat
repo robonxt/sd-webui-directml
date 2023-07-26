3703200b4397802dd9e7dbc4fd2aeb8a28abb2bc
robonxt <8854735+robonxt@users.noreply.github.com>
2023-07-26 13:47:32 -0700

    update
    Signed-off-by: robonxt <8854735+robonxt@users.noreply.github.com>

--- webui-user.bat
+++ webui-user.bat
@@ -5,6 +5,5 @@ set GIT=
 set VENV_DIR=
 set SAFETENSORS_FAST_GPU=1
 set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6,max_split_size_mb:128
-set COMMANDLINE_ARGS=--backend directml --disable-nan-check --skip-torch-cuda-test --opt-sub-quad-attention --no-download-sd-model --enable-insecure-extension-access --medvram --precision autocast --upcast-sampling --use-cpu CLIP BLIP interrogate gfpgan bsrgan esrgan scunet codeformer --sub-quad-q-chunk-size 512 --sub-quad-kv-chunk-size 512 --sub-quad-chunk-threshold 80 --update-all-extensions --skip-version-check --listen --port 7860
-
+set COMMANDLINE_ARGS=--backend directml --disable-nan-check --enable-insecure-extension-access  --listen --no-download-sd-model --opt-sub-quad-attention --precision autocast  --skip-torch-cuda-test --skip-version-check --sub-quad-chunk-threshold 80 --sub-quad-kv-chunk-size 512 --sub-quad-q-chunk-size 512 --upcast-sampling  --update-all-extensions --use-cpu CLIP BLIP INTERROGATE GFPGAN BSRGAN ESRGAN SCUNet CodeFormer 
 call webui.bat %*
